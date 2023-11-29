959b8b1b6ca6178f997e237677efeb985c6b2c5f
ADITYA_MISHRA <90920322+Adityamishra45@users.noreply.github.com>
2023-11-29 22:34:30 +0530

    Update Day_80.java

--- Day_80.java
+++ Day_80.java
@@ -1,13 +1,14 @@
-public int isEulerCircuit(int V, List<Integer>[] adj) 
+class Solution{
+    public int isEulerCircuit(int V, List<Integer>[] adj) 
     {
         // code here
-       int oddCount = 0;
-    for(int i=0;i<V;i++){ 
-        if(adj[i].size()%2!=0){
-            oddCount+=1;
+       int e=0,od=0;
+       for(int i=0;i<V;i++)
+           if(adj[i].size()%2==0)
+               e++;
+           else od++;
+       if(e==V)return 2;
+       if(od==2)return 1;
+       return 0;
     }
-    }
-    if(oddCount==0)return 2;
-    else if(oddCount==2)return 1;
-    else return 0;
-    }
+}
