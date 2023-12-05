36236a9a82a3ff6ec7c14aca83fa6007b19f9069
ADITYA_MISHRA <90920322+Adityamishra45@users.noreply.github.com>
2023-12-05 22:07:50 +0530

    Update Day_87.java

--- Day_87.java
+++ Day_87.java
@@ -1,18 +1,21 @@
 class Solution {
     int getMinDiff(int[] arr, int n, int k) {
         // code here
-        sort(arr,arr+n);
-       int minele,maxele;
-       int result=arr[n-1]-arr[0];
-       
-       for(int i=1;i<n;i++){
-           if(arr[i]>=k){
-               maxele=max(arr[i-1]+k,arr[n-1]-k);
-               minele=min(arr[0]+k,arr[i]-k);
-               result=min(result,maxele-minele);
-           }
-           
-       }
-       return result;
+        Arrays.sort(arr);
+        
+        int ans = arr[n-1] - arr[0];
+        
+        int tempmin = arr[0];
+        int tempmax = arr[n-1];
+        
+        for(int i=1;i<n;i++) {
+            if(arr[i] - k < 0) {
+                continue;
+            }
+            tempmin = Math.min(arr[0] + k, arr[i] - k);
+            tempmax = Math.max(arr[i-1] + k , arr[n-1] - k);
+            ans = Math.min(ans, tempmax - tempmin);
+        }
+        return ans;
     }
 }
